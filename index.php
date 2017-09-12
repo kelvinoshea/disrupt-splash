e2c09b9189d154f42ed3d30b7da05debdef661d3
kelvinoshea <kelvin.oshea@hotmail.com>
2017-09-12 11:44:17 +1000

    edited index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- GENERIC META -->
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta http-equiv='content-type' content='text/html; charset=utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, minimal-ui, user-scalable=0'>
+
+    <!-- GENERIC STYLES -->
+    <link type="text/css" rel="style.css">
+
+    <!-- GENERIC SCRIPTS -->
+    <script src="script.js"></script>
+</head>
+    <body>
+        
+    </body>
+</html>
