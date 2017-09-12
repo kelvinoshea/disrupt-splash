023db2e8e1a406676ab46984c6bdfaee579a2f1e
Mitchell Williams <mwm1503@gmail.com>
2017-09-12 22:07:31 +1000

    ajax + small stuff

---
+++ app/scripts/main.js
@@ -0,0 +1,48 @@
+$(function() {
+  // We Ready Boiis
+
+  $('#formEmail').submit(function() {
+    $.ajax({
+    type: 'POST',
+    url: '',
+    data: { email: $('#formEmail .email').val()},
+    secure: true,
+    tryCount: 0,
+    retryLimit: 3,
+    beforeSend: function() {
+
+    },
+    error: function(jqXHR, textStatus, error) {
+      if (textStatus === 'timeout') {
+        this.tryCount++;
+        if (this.tryCount <= this.retryLimit) {
+            // try again
+            $.ajax(this);
+            return;
+        }
+        console.log("Arax Request Failed:");
+        console.log(jqXHR.responseText);
+        console.log(textStatus);
+        console.log(error);
+        console.log("Query: " + query);
+        console.log("--------------------");
+      } else {
+        console.log("Arax Request Failed:");
+        console.log(jqXHR.responseText);
+        console.log(textStatus);
+        console.log(error);
+        console.log("Query: " + query);
+        console.log("--------------------");
+      }
+    },
+    success: function() {
+      $('#formEmail .buttonText').html("Thank You");
+    }
+  });
+
+
+
+
+    return false;
+  });
+});
