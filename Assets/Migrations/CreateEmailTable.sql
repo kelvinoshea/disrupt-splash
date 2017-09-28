e08923cd90c459fd7a1ca316a1dc106ef6c79a0d
Mitchell Williams <mwm1503@gmail.com>
2017-09-28 21:27:14 +1000

    generic databse name

--- Assets/Migrations/CreateEmailTable.sql
+++ Assets/Migrations/CreateEmailTable.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `mitchell_dev_straighttalk`.`Emails` ( `id` INT(4) NOT NULL AUTO_INCREMENT , `email` VARCHAR(30) NOT NULL , `submitDate` DATETIME NOT NULL , PRIMARY KEY (`id`), UNIQUE (`email`)) ENGINE = MyISAM;
+CREATE TABLE `DATABASE`.`Emails` ( `id` INT(4) NOT NULL AUTO_INCREMENT , `email` VARCHAR(30) NOT NULL , `submitDate` DATETIME NOT NULL , PRIMARY KEY (`id`), UNIQUE (`email`)) ENGINE = MyISAM;
