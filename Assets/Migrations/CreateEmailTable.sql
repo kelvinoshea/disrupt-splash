dceead0f063241b6cefe0bc5eabf34461934bcd3
Mitchell Williams <mwm1503@gmail.com>
2017-09-28 21:25:10 +1000

    add migrations

---
+++ Assets/Migrations/CreateEmailTable.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `mitchell_dev_straighttalk`.`Emails` ( `id` INT(4) NOT NULL AUTO_INCREMENT , `email` VARCHAR(30) NOT NULL , `submitDate` DATETIME NOT NULL , PRIMARY KEY (`id`), UNIQUE (`email`)) ENGINE = MyISAM;
