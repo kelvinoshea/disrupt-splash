cc4ec12c1e78e53406089c6f1902c1e4485e49a2
smhdale <smhdale@live.com.au>
2017-10-08 15:13:57 +1000

    Fixed colours, added subtle animation

--- app/scripts/nameBackground.js
+++ app/scripts/nameBackground.js
@@ -41,8 +41,15 @@ class NameBackground {
       'Mitchell Williams',
       'Monica Wong'
     ]
-    this.names = null
-    this.listMultiplier = 3
+    this.names = []
+
+    // A full set of names will be created for each colour
+    // Layered in the same order
+    this.colours = [
+      '181D38',
+      '1C2242',
+      '20284C',
+    ]
 
     this.w = window.innerWidth
     this.h = window.innerHeight
@@ -65,9 +72,11 @@ class NameBackground {
     this.makeNameObjects()
 
     for (let o of this.names) {
-      o.update()
       o.draw()
     }
+
+    // Start animation
+    this.startAnim()
   }
 
   initCanvas () {
@@ -78,21 +87,17 @@ class NameBackground {
     this.canvas.style.width = '100vw'
     this.canvas.style.height = '100vh'
 
+    // Tweak bg visibility here
+    this.canvas.style.opacity = 1
+
     this.targetNode.insertBefore(this.canvas, this.targetNode.firstChild)
   }
 
   makeNameObjects () {
     // DisruptName(canvas, text, fontSize, heightOffsetPct)
-    let endList = []
-
-    for (let i = 0; i < this.listMultiplier; i++) {
-      endList = [
-        ...endList,
-        ...this.nameStrings
-      ]
+    for (let i = 0; i < this.colours.length; i++) {
+      this.names.push(...this.nameStrings.map(name => new DisruptName(this.canvas, name, this.fontSize, this.colours[i], 0.2)))
     }
-
-    this.names = endList.map(name => new DisruptName(this.canvas, name, this.fontSize, 0.2))
   }
 
   // DRAW FUNCTIONS
@@ -102,33 +107,66 @@ class NameBackground {
   }
 
   animate () {
-    this.drawNames()
+    let update = false
+    for (let name of this.names) {
+      update = update || name.update()
+    }
+
+    // Redraw if at least one name updated
+    if (update) {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      for (let name of this.names) {
+        name.draw()
+      }
+    }
+
+    // Schedule next update
     window.requestAnimationFrame(this.animate.bind(this))
   }
 }
 
+function randBetween(min, max) {
+  return min + Math.random() * (max - min + 1)
+}
+
+function randInt(min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1))
+}
+
 class DisruptName {
-  constructor (canvas, name, height, offsetPct) {
+  constructor (canvas, name, height, colour, offsetPct) {
     this.name = name.split('')
     this.length = this.name.length
-    this.offsets = new Array(this.length).fill(0)
     this.x = 0
     this.y = 0
-    this.yStart = 0
 
     this.canvas = canvas
     this.ctx = canvas.getContext('2d')
 
-    this.ctx.fillStyle = '#f00'
+    this.colour = colour
     this.ctx.font = `${height}px disruptheavy`
 
-    this.w = this.ctx.measureText(name).width
-    this.baseH = height
-    this.h = height
+    this.offset = new Array(this.length).fill(0)
     this.offsetPct = offsetPct
-    this.offsetChance = 0.2 // min 0, max 1
+    this.offsetChance = 0.15 // min 0, max 1
+    this.offsetBound = 2
+
+    this.w = this.ctx.measureText(name).width
+
+    this.baseH = height
+    this.h = height * (1 + this.offsetBound*2 * this.offsetPct)
+    this.yStart = this.offsetPct * this.offsetBound
 
     this.letterWidths = this.name.map(this.getLetterWidth.bind(this))
+
+    // Update time range, in seconds
+    this.updateMin = 5
+    this.updateMax = 30
+    this.setUpdateCounter()
+
+    // Set pos on canvas
+    this.setOffsets()
+    this.setPos()
   }
 
   getLetterWidth (char) {
@@ -137,63 +175,53 @@ class DisruptName {
 
   makeOffset () {
     let chance = Math.random()
-    if (chance <= this.offsetChance / 2) {
-      return -1
-    } else if (chance > 1 - this.offsetChance / 2) {
-      return 1
+    let out = 0
+
+    if (chance <= this.offsetChance) {
+      out = 1 * (Math.random() > 0.5 ? 1 : -1)
     }
-    return 0
+    return out
   }
 
   setOffsets () {
+    let current = randInt(-1, 1)
     for (let i = 0; i < this.length; i++) {
-      this.offsets[i] = this.makeOffset()
-    }
-  }
-
-  calcTotalHeight () {
-    let up = 0
-    let down = 0
-    let current = 0
-
-    // Get highest and lowest offsets
-    for (let o of this.offsets) {
-      current += o
-      up = Math.max(up, current)
-      down = Math.min(down, current)
-    }
-
-    // Convert to physical height as percentage
-    return {
-      h: 1 + (up * this.offsetPct) + (-down * this.offsetPct),
-      start: up * this.offsetPct
+      this.offset[i] = current
+      current = Math.max(-this.offsetBound, Math.min(current + this.makeOffset(), this.offsetBound))
     }
   }
 
   setPos () {
-    // Recalculate height since offsets have changed
-    let hCalc = this.calcTotalHeight()
-    this.h = this.baseH * hCalc.h
-    this.yStart = this.baseH * hCalc.start
-
     // Place on canvas
     this.x = Math.random() * (this.canvas.width - this.w)
-    this.y = Math.random() * (this.canvas.height - this.h  - this.baseH)
+    this.y = Math.random() * (this.canvas.height - this.h - this.baseH)
+  }
+
+  setUpdateCounter () {
+    // Be lazy and assume ~60 FPS
+    let FPS = 60
+    this.updateCounter = Math.floor(randBetween(this.updateMin, this.updateMax) * FPS)
   }
 
   update () {
-    this.setOffsets()
-    this.setPos()
+    // Return true if update happened
+    // so parent can redraw canvas
+    if (--this.updateCounter === 0) {
+      this.setOffsets()
+      this.setUpdateCounter()
+      return true
+    }
+    return false
   }
 
   draw () {
+    this.ctx.fillStyle = `#${this.colour}`
     let dx = 0
-    let dy = this.yStart
+    let dy = 0
     for (let i = 0; i < this.length; i++) {
+      dy = this.offset[i] * this.offsetPct * this.baseH
       this.ctx.fillText(this.name[i], this.x + dx, this.y + dy + this.baseH)
-
       dx += this.letterWidths[i]
-      dy += this.offsets[i] * this.offsetPct * this.baseH
     }
   }
 }
