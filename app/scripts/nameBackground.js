76d646e1540edd0986aa089c31ea191d3846ad4a
smhdale <smhdale@live.com.au>
2017-10-10 00:57:03 +1000

    Ok let's take a different approach

--- app/scripts/nameBackground.js
+++ app/scripts/nameBackground.js
@@ -2,7 +2,7 @@
 
 class NameBackground {
   constructor (target) {
-    this.nameStrings = [
+    this.names = [
       'Elouise Baartz',
       'Amira Afiqa Binti Azimi',
       'Joelle Black',
@@ -41,19 +41,16 @@ class NameBackground {
       'Mitchell Williams',
       'Monica Wong'
     ]
-    this.names = []
-
-    // A full set of names will be created for each colour
-    // Layered in the same order
-    this.colours = [
-      '181D38',
-      '1C2242',
-      '20284C',
-    ]
 
     this.w = window.innerWidth
     this.h = window.innerHeight
-    this.fontSizePct = 4 // x% of minimum browser width/height
+    this.fontSizePct = 2 // x% of minimum browser width/height
+    this.lineHeightMult = 1.5
+
+    this.textLines = []
+    this.textBgCoverage = 0.6
+
+    this.srcImage = new Image()
 
     this.canvas = document.createElement('canvas')
     this.ctx = this.canvas.getContext('2d')
@@ -65,15 +62,30 @@ class NameBackground {
     return Math.min(this.w, this.h) * this.fontSizePct / 100
   }
 
+  get numLines () {
+    return Math.floor(this.h * this.textBgCoverage / this.lineHeight)
+  }
+
+  get lineHeight () {
+    return this.fontSize * this.lineHeightMult
+  }
+
+  get imgHeight () {
+    return this.numLines * this.lineHeight
+  }
+
   // INIT FUNCTIONS
 
   init () {
     this.initCanvas()
-    this.makeNameObjects()
-
-    for (let o of this.names) {
-      o.draw()
-    }
+    this.makeTextLines()
+    this.makeSrcImage().then(img => {
+      return this.disruptSrcImage(img)
+    }).then(() => {
+      this.ctx.drawImage(this.srcImage, 0, 0)
+    }).catch(() => {
+      console.log('Error setting up background image.')
+    })
 
     // Start animation
     this.startAnim()
@@ -84,20 +96,119 @@ class NameBackground {
     this.canvas.width = this.w
     this.canvas.height = this.h
     this.canvas.style.position = 'absolute'
-    this.canvas.style.width = '100vw'
+    this.canvas.style.left = '-1vw'
+    this.canvas.style.width = '102vw'
     this.canvas.style.height = '100vh'
-
-    // Tweak bg visibility here
     this.canvas.style.opacity = 1
+
+    this.ctx.font = `${this.fontSize}px disruptheavy`
+    this.ctx.fillStyle = '#20284C'
 
     this.targetNode.insertBefore(this.canvas, this.targetNode.firstChild)
   }
 
-  makeNameObjects () {
-    // DisruptName(canvas, text, fontSize, heightOffsetPct)
-    for (let i = 0; i < this.colours.length; i++) {
-      this.names.push(...this.nameStrings.map(name => new DisruptName(this.canvas, name, this.fontSize, this.colours[i], 0.2)))
+  joinNames (names) {
+    return names.map(n => n.toUpperCase().split('').join(' ')).join('  •  ')
+  }
+
+  makeTextLines() {
+    let names = []
+    let namesLen
+
+    let thisName
+    let thisLine = []
+    let thisLineLength
+
+    while (this.textLines.length < this.numLines) {
+      // Generate a full line of text
+      thisLine = []
+      thisLineLength = 0
+
+      while (thisLineLength < window.innerWidth) {
+        // Refill names array if needed
+        if (names.length === 0) {
+          names = this.names.slice()
+        }
+
+        // Choose a random name
+        thisName = names.splice(randInt(0, names.length - 1), 1)[0]
+        thisLine.push(thisName)
+        thisLineLength = this.ctx.measureText(this.joinNames(thisLine)).width
+      }
+
+      this.textLines.push(this.joinNames(thisLine))
     }
+  }
+
+  makeSrcImage () {
+    this.clearCanvas()
+
+    let textY = (this.h - this.imgHeight) / 2
+    let textX
+
+    for (let line of this.textLines) {
+      textX = (this.w - this.ctx.measureText(line).width) / 2
+      this.ctx.fillText(line, textX, textY + this.fontSize)
+      textY += this.lineHeight
+    }
+
+    let png = this.canvas.toDataURL()
+    this.clearCanvas()
+
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => {
+        resolve(img)
+      }
+      img.onerror = reject
+      img.src = png
+    })
+  }
+
+  disruptSrcImage (img) {
+    this.clearCanvas()
+
+    let imgY = (this.h - this.imgHeight) / 2
+    let fineTune = 0.08
+    let lineAdjust = this.lineHeight * (0.5 + fineTune)
+
+    let dx = 0
+    let dxMax = this.fontSize
+
+    let dyOld = 0
+    let dy = 0
+    let dyMax = this.fontSize / 4
+
+    let rectY
+
+    for (let i = 0; i < this.numLines + 1; i++) {
+      // Choose y offset for this line
+      dy = randInt(-dyMax, dyMax)
+
+      // Draw line
+      dx = randInt(-dxMax, dxMax)
+      rectY = imgY + i * this.lineHeight - lineAdjust
+      this.ctx.drawImage(
+        img,
+        0, rectY + dyOld, this.w, this.lineHeight + dy - dyOld,
+        dx, rectY + dyOld, this.w, this.lineHeight + dy - dyOld
+      )
+
+      dyOld = dy
+    }
+
+    let png = this.canvas.toDataURL()
+    this.clearCanvas()
+
+    return new Promise((resolve, reject) => {
+      this.srcImage.onload = resolve
+      this.srcImage.onerror = resolve
+      this.srcImage.src = png
+    })
+  }
+
+  clearCanvas () {
+    this.ctx.clearRect(0, 0, this.w, this.h)
   }
 
   // DRAW FUNCTIONS
@@ -107,18 +218,6 @@ class NameBackground {
   }
 
   animate () {
-    let update = false
-    for (let name of this.names) {
-      update = update || name.update()
-    }
-
-    // Redraw if at least one name updated
-    if (update) {
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-      for (let name of this.names) {
-        name.draw()
-      }
-    }
 
     // Schedule next update
     window.requestAnimationFrame(this.animate.bind(this))
@@ -133,116 +232,7 @@ function randInt(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1))
 }
 
-class DisruptName {
-  constructor (canvas, name, height, colour, offsetPct) {
-    this.name = name.split('')
-    this.length = this.name.length
-    this.x = 0
-    this.y = 0
-
-    this.canvas = canvas
-    this.ctx = canvas.getContext('2d')
-
-    this.colour = colour
-    this.ctx.font = `${height}px disruptheavy`
-
-    this.offset = new Array(this.length).fill(0)
-    this.offsetPct = offsetPct
-    this.offsetChance = 0.15 // min 0, max 1
-    this.offsetBound = 2
-
-    this.w = this.ctx.measureText(name).width
-
-    this.baseH = height
-    this.h = height * (1 + this.offsetBound*2 * this.offsetPct)
-    this.yStart = this.offsetPct * this.offsetBound
-
-    this.letterWidths = this.name.map(this.getLetterWidth.bind(this))
-
-    // Update time range, in seconds
-    this.updateMin = 5
-    this.updateMax = 30
-    this.setUpdateCounter()
-
-    // Set pos on canvas
-    this.setOffsets()
-    this.setPos()
-  }
-
-  getLetterWidth (char) {
-    return this.ctx.measureText(char).width
-  }
-
-  makeOffset () {
-    let chance = Math.random()
-    let out = 0
-
-    if (chance <= this.offsetChance) {
-      out = 1 * (Math.random() > 0.5 ? 1 : -1)
-    }
-    return out
-  }
-
-  setOffsets () {
-    let current = randInt(-1, 1)
-    for (let i = 0; i < this.length; i++) {
-      this.offset[i] = current
-      current = Math.max(-this.offsetBound, Math.min(current + this.makeOffset(), this.offsetBound))
-    }
-  }
-
-  _setXY () {
-    this.x = Math.random() * (this.canvas.width - this.w)
-    this.y = Math.random() * (this.canvas.height - this.h)
-  }
-
-  setPos () {
-    if (IS_MOBILE) {
-      // Don't worry about ignoring orb on mobile
-      this._setXY()
-    } else {
-      // Place on canvas
-      // Let's try and keep stuff out from behind the orb
-      let orbSide = Math.min(this.canvas.width, this.canvas.height) * .65
-      let orbX = (this.canvas.width - orbSide) / 2
-      let orbY = (this.canvas.height - orbSide) / 2
-
-      do {
-        this._setXY()
-      } while (
-        (this.x > (orbX - this.w) && this.x < orbX + orbSide &&
-        this.y > (orbY - this.h) && this.y < orbY + orbSide)
-      )
-    }
-  }
-
-  setUpdateCounter () {
-    // Be lazy and assume ~60 FPS
-    let FPS = 60
-    this.updateCounter = Math.floor(randBetween(this.updateMin, this.updateMax) * FPS)
-  }
-
-  update () {
-    // Return true if update happened
-    // so parent can redraw canvas
-    if (--this.updateCounter === 0) {
-      this.setOffsets()
-      this.setUpdateCounter()
-      return true
-    }
-    return false
-  }
-
-  draw () {
-    this.ctx.fillStyle = `#${this.colour}`
-    let dx = 0
-    let dy = 0
-    for (let i = 0; i < this.length; i++) {
-      dy = this.offset[i] * this.offsetPct * this.baseH
-      this.ctx.fillText(this.name[i], this.x + dx, this.y + dy + this.baseH)
-      dx += this.letterWidths[i]
-    }
-  }
+// This effect is desktop only
+if (!IS_MOBILE) {
+  window.NAME_BG = new NameBackground('main.fullScreenLayout')
 }
-
-window.NAME_BG = new NameBackground('main.fullScreenLayout')
